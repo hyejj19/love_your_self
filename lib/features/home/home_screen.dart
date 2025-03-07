dffcda592155153ed3e99b7dd056cd4652d7ac11
hyejj19@naver.com <hyejj19@naver.com>
2025-03-07 15:41:51 +0900

    add: home screen list view

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:love_your_self/mock/mock_mood_list.dart';
 
 class HomeScreen extends StatefulWidget {
   static String routePath = '/home';
@@ -11,15 +13,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, dynamic>> _moodList = [];
+
+  String _getMoodIcon(String mood) {
+    if (mood == 'EXCITED') return '🤩';
+    if (mood == 'HAPPY') return '🥰';
+    if (mood == 'NEUTRAL') return '😌';
+    if (mood == 'SAD') return '😭';
+    if (mood == 'ANGRY') return '🤬';
+    if (mood == 'TIRED') return '🫠';
+
+    return '👻';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Mood list'),
+      ),
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(
           horizontal: 20,
         ),
-        child: Column(),
+        child: ListView.separated(
+          scrollDirection: Axis.vertical,
+          separatorBuilder: (context, index) => const Gap(10),
+          itemCount: MOCK_MOOD_LIST.length,
+          itemBuilder: (context, index) {
+            final mood = MOCK_MOOD_LIST[index % MOCK_MOOD_LIST.length];
+
+            return ListTile(
+              title: Text(mood["content"] ?? "No content"),
+              subtitle: Text(mood["created"] ?? ""),
+              leading: Text(
+                _getMoodIcon(mood["mood"]),
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            );
+          },
+        ),
       )),
     );
   }
---
+++ lib/mock/mock_mood_list.dart
@@ -0,0 +1,32 @@
+List MOCK_MOOD_LIST = [
+  {
+    "mood": "EXCITED",
+    "content": "완전 신나!!",
+    "created": "2025-03-07T12:34:56Z",
+  },
+  {
+    "mood": "HAPPY",
+    "content": "오늘은 기분이 좋음",
+    "created": "2025-03-07T12:34:56Z",
+  },
+  {
+    "mood": "NEUTRAL",
+    "content": "오늘은 기분이 평온",
+    "created": "2025-03-07T12:34:56Z",
+  },
+  {
+    "mood": "SAD",
+    "content": "엉엉",
+    "created": "2025-03-07T12:34:56Z",
+  },
+  {
+    "mood": "ANGRY",
+    "content": "우씨 화나",
+    "created": "2025-03-07T12:34:56Z",
+  },
+  {
+    "mood": "TIRED",
+    "content": "피곤하당..",
+    "created": "2025-03-07T12:34:56Z",
+  },
+];
